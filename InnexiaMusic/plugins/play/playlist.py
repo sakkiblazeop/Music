f66190e1f068d5bb412ce7ee2abfc8be88ab8ee4
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2023-01-20 11:21:04 +0530

    Update playlist.py

--- InnexiaMusic/plugins/play/playlist.py
+++ InnexiaMusic/plugins/play/playlist.py
@@ -48,7 +48,7 @@ async def check_playlist(client, message: Message, _):
         count += 1
         msg += f"\n\n{count}- {title[:70]}\n"
         msg += _["playlist_5"].format(duration)
-    link = await Anonbin(msg)
+    link = await Pastebin(msg)
     lines = msg.count("\n")
     if lines >= 17:
         car = os.linesep.join(msg.split(os.linesep)[:17])
