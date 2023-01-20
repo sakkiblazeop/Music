6ab8a9deca7f50089ce16fc66d211afd732fd13e
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2023-01-20 16:38:50 +0530

    Update start.py

--- InnexiaMusic/plugins/bot/start.py
+++ InnexiaMusic/plugins/bot/start.py
@@ -13,8 +13,8 @@ from config import OWNER_ID
 from strings import get_command, get_string
 from InnexiaMusic import Telegram, YouTube, app
 from InnexiaMusic.misc import SUDOERS, _boot_
-from InnexiaMusic.plugins.playlist import del_plist_msg
-from InnexiaMusic.plugins.sudoers import sudoers_list
+from InnexiaMusic.plugins.play.playlist import del_plist_msg
+from InnexiaMusic.plugins.sudo.sudoers import sudoers_list
 from InnexiaMusic.utils.database import (add_served_chat,
                                        add_served_user,
                                        get_served_chats,
