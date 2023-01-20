d31d85941cd1ccbe1ff57d6dad7b69e72d8c168b
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2023-01-20 11:38:17 +0530

    Update __init__.py

--- InnexiaMusic/__init__.py
+++ InnexiaMusic/__init__.py
@@ -1,1 +1,37 @@
+from callsmusic.bot import InnexiaBot
+from InnexiaMusic.core.dir import dirr
+from InnexiaMusic.core.git import git
+from callsmusic.userbot import Userbot
+from InnexiaMusic.misc import dbb, heroku, sudo
+from aiohttp import ClientSession
 
+from .logging import LOGGER
+
+
+dirr()
+
+git()
+
+dbb()
+
+heroku()
+
+sudo()
+
+# Clients
+app = InnexiaBot()
+
+userbot = Userbot()
+
+
+from .platforms import *
+
+YouTube = YouTubeAPI()
+Carbon = CarbonAPI()
+Spotify = SpotifyAPI()
+Apple = AppleAPI()
+Resso = RessoAPI()
+SoundCloud = SoundAPI()
+Telegram = TeleAPI()
+
+aiohttpsession = ClientSession()
