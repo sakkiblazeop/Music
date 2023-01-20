3011b02c87bd5b05605a256bfe21176df441e63b
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2023-01-20 12:39:51 +0530

    Update help.py

--- InnexiaMusic/plugins/bot/help.py
+++ InnexiaMusic/plugins/bot/help.py
@@ -3,7 +3,6 @@ from typing import Union
 from pyrogram import filters, types
 from pyrogram.types import InlineKeyboardMarkup, Message
 
-import config
 from config import BANNED_USERS
 from strings import get_command, get_string, helpers
 from InnexiaMusic import app
@@ -42,12 +41,13 @@ async def helper_private(
         _ = get_string(language)
         keyboard = help_pannel(_, True)
         if update.message.photo:
-            await update.edit_message_text(
-                _["help_1"].format(config.SUPPORT_HEHE), reply_markup=keyboard
+            await update.message.delete()
+            await update.message.reply_text(
+                _["help_1"], reply_markup=keyboard
             )
         else:
             await update.edit_message_text(
-                _["help_1"].format(config.SUPPORT_HEHE), reply_markup=keyboard
+                _["help_1"], reply_markup=keyboard
             )
     else:
         chat_id = update.chat.id
@@ -59,9 +59,7 @@ async def helper_private(
         language = await get_lang(chat_id)
         _ = get_string(language)
         keyboard = help_pannel(_)
-        await update.reply_photo(
-            photo=config.START_IMG_URL,
-            caption=_["help_1"].format(config.SUPPORT_HEHE), reply_markup=keyboard)
+        await update.reply_text(_["help_1"], reply_markup=keyboard)
 
 
 @app.on_message(
@@ -73,9 +71,8 @@ async def helper_private(
 @LanguageStart
 async def help_com_group(client, message: Message, _):
     keyboard = private_help_panel(_)
-    await message.reply_photo(
-        photo=config.START_IMG_URL,
-        caption=_["help_2"], reply_markup=InlineKeyboardMarkup(keyboard)
+    await message.reply_text(
+        _["help_2"], reply_markup=InlineKeyboardMarkup(keyboard)
     )
 
 
@@ -85,14 +82,14 @@ async def helper_cb(client, CallbackQuery, _):
     callback_data = CallbackQuery.data.strip()
     cb = callback_data.split(None, 1)[1]
     keyboard = help_back_markup(_)
-    if cb == "hb9":
+    if cb == "hb5":
         if CallbackQuery.from_user.id not in SUDOERS:
             return await CallbackQuery.answer(
-                "This button is only for sudoers.", show_alert=True
+                "Only for Sudo Users", show_alert=True
             )
         else:
             await CallbackQuery.edit_message_text(
-                helpers.HELP_9, reply_markup=keyboard
+                helpers.HELP_5, reply_markup=keyboard
             )
             return await CallbackQuery.answer()
     try:
@@ -114,32 +111,4 @@ async def helper_cb(client, CallbackQuery, _):
     elif cb == "hb4":
         await CallbackQuery.edit_message_text(
             helpers.HELP_4, reply_markup=keyboard
-        )
-    elif cb == "hb5":
-        await CallbackQuery.edit_message_text(
-            helpers.HELP_5, reply_markup=keyboard
-        )
-    elif cb == "hb6":
-        await CallbackQuery.edit_message_text(
-            helpers.HELP_6, reply_markup=keyboard
-        )
-    elif cb == "hb7":
-        await CallbackQuery.edit_message_text(
-            helpers.HELP_7, reply_markup=keyboard
-        )
-    elif cb == "hb8":
-        await CallbackQuery.edit_message_text(
-            helpers.HELP_8, reply_markup=keyboard
-        )
-    elif cb == "hb10":
-        await CallbackQuery.edit_message_text(
-            helpers.HELP_10, reply_markup=keyboard
-        )
-    elif cb == "hb11":
-        await CallbackQuery.edit_message_text(
-            helpers.HELP_11, reply_markup=keyboard
-        )
-    elif cb == "hb12":
-        await CallbackQuery.edit_message_text(
-            helpers.HELP_12, reply_markup=keyboard
         )
