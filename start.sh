ea4a438356f962e35bcc6b3ae792973b96f71a64
sakkiblazeop <123486210+sakkiblazeop@users.noreply.github.com>
2023-01-25 12:34:48 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /app
 echo "----- Now deployed web booting your repo ------ " 
-git clone https://github.com/Team-Deadly/Music && cd Music && pip3 install -U -r requirements.txt
+cd Music && pip3 install -U -r requirements.txt
 gunicorn app:app & python3 -m InnexiaMusic
