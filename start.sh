07c52c58ccb7248004ca658082e375b2557d35cf
sakkiblazeop <123486210+sakkiblazeop@users.noreply.github.com>
2023-01-25 12:17:58 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-source /venv/bin/activate
 cd /app
-
-python -m InnexiaMusic
+echo "----- Now deployed web booting your repo ------ " 
+git clone https://github.com/Team-Deadly/Music && cd Music && pip3 install -U -r requirements.txt
+gunicorn app:app & python3 -m InnexiaMusic
