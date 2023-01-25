fb5728ca2fca4e24019d35e99553cb5205e02bb8
sakkiblazeop <123486210+sakkiblazeop@users.noreply.github.com>
2023-01-25 13:53:21 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 cd /app
 echo "----- Now deployed web booting your repo ------ " 
-git clone $REPO_URL ok && cd ok && pip3 install -U -r requirements.txt
 gunicorn app:app & python3 -m InnexiaMusic
